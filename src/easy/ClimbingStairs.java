bb0a1f219587a5ee52a80e2896a77c9494ea3141
ali hamzayev <008461@ibar.int>
2024-11-17 23:09:55 +0400

    solve climbing stairs with solution online.

---
+++ src/easy/ClimbingStairs.java
@@ -0,0 +1,19 @@
+package easy;
+
+public class ClimbingStairs {
+    public static void main(String[] args) {
+        System.out.println(new ClimbingStairs().climbStairs(35));
+    }
+
+    public int climbStairs(int n) {
+        int[] dp = new int[n + 1];
+        dp[0] = 1;
+        dp[1] = 2;
+
+        for (int i = 2; i < n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+
+        return dp[n - 1];
+    }
+}
